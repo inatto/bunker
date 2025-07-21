f9b638de160e52699558bf7547707f94608efaef
Daniel Maia <danielmaiax@gmail.com>
2025-07-20 23:24:20 -0300

    ...

---
+++ src/Core/IpHelper.php
@@ -0,0 +1,35 @@
+<?php /** @noinspection PhpUnused */
+
+namespace Inatto\Bunker\Core;
+
+
+class IpHelper
+{
+    public static function getClientIp(): string
+    {
+        $ipHeaders = [
+            'HTTP_CLIENT_IP',
+            'HTTP_X_FORWARDED_FOR',
+            'HTTP_X_FORWARDED',
+            'HTTP_X_CLUSTER_CLIENT_IP',
+            'HTTP_FORWARDED_FOR',
+            'HTTP_FORWARDED',
+            'REMOTE_ADDR'
+        ];
+
+        foreach ($ipHeaders as $header) {
+            if (!empty($_SERVER[$header])) {
+                $ips = explode(',', $_SERVER[$header]);
+                foreach ($ips as $ip) {
+                    $ip = trim($ip);
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+                        return $ip;
+                    }
+                }
+            }
+        }
+
+        //
+        return '0.0.0.0';
+    }
+}
